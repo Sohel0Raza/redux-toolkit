43c952051284afd40d130bbc2cc04f024177b972
Sohel0Raza <soheldot1239@gmail.com>
2024-08-21 15:08:54 +0600

    create core exmple use redux

---
+++ index.js
@@ -0,0 +1,62 @@
+import { createStore } from "redux";
+
+const INCREMENT = "INCREMENT";
+const DECREMENT = "DECREMENT";
+const RESET = "RESET";
+
+//state
+const initialState = {
+  count: 0,
+};
+
+//action
+const incrementCounterAction = () => {
+  return { type: INCREMENT };
+};
+
+const decrementCounterAction = () => {
+  return { type: DECREMENT };
+};
+const ressetAction = () => {
+  return { type: RESET };
+};
+
+//reducer
+
+const counterReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case INCREMENT:
+      return {
+        ...state,
+        count: state.count + 1,
+      };
+
+    case DECREMENT:
+      return {
+        ...state,
+        count: state.count - 1,
+      };
+
+    case RESET:
+      return {
+        ...state,
+        count: 0,
+      };
+
+    default:
+      state;
+  }
+};
+
+const store = createStore(counterReducer);
+
+store.subscribe(() => {
+  console.log(store.getState());
+});
+
+store.dispatch(incrementCounterAction());
+store.dispatch(incrementCounterAction());
+store.dispatch(decrementCounterAction());
+store.dispatch(incrementCounterAction());
+store.dispatch(incrementCounterAction());
+store.dispatch(ressetAction());
